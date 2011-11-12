a09195a760fd1c45981a1fa321dad212edcd0cc1
Craig McCulloch <pyridoxus@localhost>
2011-11-12 14:36:18 -0600

    Made all spheres similar to what assignment image indicates.

--- lab5.cpp
+++ lab5.cpp
@@ -27,19 +27,22 @@ void myInit()
    glMatrixMode(GL_MODELVIEW);
    glLoadIdentity();
    glTranslatef( 0, 0, -25 );
-   glClearColor( 0.0, 0.0, 0.0, 0.0 );
+   glClearColor( 1.0, 1.0, 0.0, 0.0 );
    glEnable( GL_DEPTH_TEST );
 
    // Select shading model
    glShadeModel(GL_SMOOTH);
-
    // Define light source
    GLfloat light_position0[] = { 0.0, 1.0, 1.0, 0.0 };
    glLightfv(GL_LIGHT0, GL_POSITION, light_position0);
 
    // Define light source
    GLfloat light_position1[] = { -1.0, -1.0, 1.0, 1.0 };
+   GLfloat light_specular1[] = { 1.0, 1.0, 1.0, 1.0 };
+   GLfloat light_diffuse1[] = { 1.0, 1.0, 1.0, 1.0 };
    glLightfv(GL_LIGHT1, GL_POSITION, light_position1);
+   glLightfv(GL_LIGHT1, GL_DIFFUSE, light_diffuse1);
+   glLightfv(GL_LIGHT1, GL_SPECULAR, light_specular1);
 
    // Enable lighting model
    glEnable(GL_LIGHTING);
@@ -54,82 +57,82 @@ void myInit()
 void myDraw()
 {
 	Sphere *sphere;
-	int j;
+	int i, j;
 	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
    // Material properties
 	float data[][4] = {// First sphere
 										 // Diffuse                 Specular
 										 { 0.0, 0.0, 0.0, 1.0 },  { 1.0, 1.0, 1.0, 1.0 },
 										 // Shininess               Translation
-			 	 	 	 	 	 	 	 { 1000.0, 0.0, 0.0, 0.0 }, { -9.0, 0.0, 0.0, 0.0 },
+			 	 	 	 	 	 	 	 { 512.0, 0.0, 0.0, 0.0 }, { -9.5, 0.0, 0.0, 0.0 },
 			 	 	 	 	 	 	 	 // Ambient
-			 	 	 	 	 	 	 	 { 0.5, 0.5, 0.5, 0.0},
+			 	 	 	 	 	 	 	 { 0.05, 0.05, 0.05, 0.0},
 										 // Second sphere
 										 // Diffuse                 Specular
-										 { 0.0, 1.0, 0.0, 1.0 },  { 1.0, 1.0, 1.0, 1.0 },
+			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.1, 1.0 },  { 0.93, 0.93, 0.95, 1.0 },
 										 // Shininess               Translation
-			 	 	 	 	 	 	 	 { 200.0, 0.0, 0.0, 0.0 }, { -7.0, 0.0, 0.0, 0.0 },
+			 	 	 	 	 	 	 	 { 256.0, 0.0, 0.0, 0.0 }, { -7.4, 0.0, 0.0, 0.0 },
 			 	 	 	 	 	 	 	 // Ambient
-			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.0, 0.0},
+			 	 	 	 	 	 	 	 { 0.1, 0.1, 0.1, 0.0},
 										 // Third sphere
 										 // Diffuse                 Specular
-										 { 0.0, 0.0, 1.0, 1.0 },  { 0.5, 0.5, 0.5, 1.0 },
+			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.2, 1.0 },  { 0.85, 0.85, 0.9, 1.0 },
 										 // Shininess               Translation
-			 	 	 	 	 	 	 	 { 100.0, 0.0, 0.0, 0.0 }, { -5.0, 0.0, 0.0, 0.0 },
+			 	 	 	 	 	 	 	 { 128.0, 0.0, 0.0, 0.0 }, { -5.3, 0.0, 0.0, 0.0 },
 			 	 	 	 	 	 	 	 // Ambient
-			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.0, 0.0},
+			 	 	 	 	 	 	 	 { 0.1, 0.1, 0.1, 0.0},
 										 // Fourth sphere
 										 // Diffuse                 Specular
-										 { 0.0, 1.0, 1.0, 1.0 },  { 0.3, 0.3, 0.3, 1.0 },
+			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.3, 1.0 },  { 0.75, 0.75, 0.85, 1.0 },
 										 // Shininess               Translation
-			 	 	 	 	 	 	 	 { 50.0, 0.0, 0.0, 0.0 }, { -3.0, 0.0, 0.0, 0.0 },
+			 	 	 	 	 	 	 	 { 64, 0.0, 0.0, 0.0 }, { -3.2, 0.0, 0.0, 0.0 },
 			 	 	 	 	 	 	 	 // Ambient
-			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.0, 0.0},
+			 	 	 	 	 	 	 	 { 0.1, 0.1, 0.1, 0.0},
 										 // Fifth sphere
 										 // Diffuse                 Specular
-										 { 1.0, 1.0, 0.0, 1.0 },  { 1.0, 0.5, 0.5, 1.0 },
+			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.4, 1.0 },  { 0.6, 0.6, 0.8, 1.0 },
 										 // Shininess               Translation
-			 	 	 	 	 	 	 	 { 75.0, 0.0, 0.0, 0.0 }, { -1.0, 0.0, 0.0, 0.0 },
+			 	 	 	 	 	 	 	 { 32.0, 0.0, 0.0, 0.0 }, { -1.1, 0.0, 0.0, 0.0 },
 			 	 	 	 	 	 	 	 // Ambient
-			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.0, 0.0},
+			 	 	 	 	 	 	 	 { 0.1, 0.1, 0.1, 0.0},
 										 // Sixth sphere
 										 // Diffuse                 Specular
-										 { 1.0, 0.0, 1.0, 1.0 },  { 0.0, 1.0, 0.0, 1.0 },
+			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.5, 1.0 },  { 0.5, 0.5, 0.75, 1.0 },
 										 // Shininess               Translation
-			 	 	 	 	 	 	 	 { 25.0, 0.0, 0.0, 0.0 }, { 1.0, 0.0, 0.0, 0.0 },
+			 	 	 	 	 	 	 	 { 26.0, 0.0, 0.0, 0.0 }, { 1.1, 0.0, 0.0, 0.0 },
 			 	 	 	 	 	 	 	 // Ambient
-			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.0, 0.0},
+			 	 	 	 	 	 	 	 { 0.1, 0.1, 0.1, 0.0},
 										 // Seventh sphere
 										 // Diffuse                 Specular
-										 { 1.0, 1.0, 1.0, 1.0 },  { 1.0, 0.0, 0.0, 1.0 },
+			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.6, 1.0 },  { 0.4, 0.4, 0.70, 1.0 },
 										 // Shininess               Translation
-			 	 	 	 	 	 	 	 { 50.0, 0.0, 0.0, 0.0 }, { 3.0, 0.0, 0.0, 0.0 },
+			 	 	 	 	 	 	 	 { 20.0, 0.0, 0.0, 0.0 }, { 3.2, 0.0, 0.0, 0.0 },
 			 	 	 	 	 	 	 	 // Ambient
-			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.0, 0.0},
+			 	 	 	 	 	 	 	 { 0.1, 0.1, 0.1, 0.0},
 										 // Eighth sphere
 										 // Diffuse                 Specular
-										 { 0.1, 0.1, 0.1, 1.0 },  { 1.0, 1.0, 1.0, 1.0 },
+			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.7, 1.0 },  { 0.3, 0.3, 0.65, 1.0 },
 										 // Shininess               Translation
-			 	 	 	 	 	 	 	 { 150.0, 0.0, 0.0, 0.0 }, { 5.0, 0.0, 0.0, 0.0 },
+			 	 	 	 	 	 	 	 { 16.0, 0.0, 0.0, 0.0 }, { 5.3, 0.0, 0.0, 0.0 },
 			 	 	 	 	 	 	 	 // Ambient
-			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.0, 0.0},
+			 	 	 	 	 	 	 	 { 0.1, 0.1, 0.1, 0.0},
 										 // Ninth sphere
 										 // Diffuse                 Specular
-										 { 0.5, 0.5, 0.5, 1.0 },  { 1.0, 0.5, 0.0, 1.0 },
+			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.8, 1.0 },  { 0.2, 0.2, 0.6, 1.0 },
 										 // Shininess               Translation
-			 	 	 	 	 	 	 	 { 100.0, 0.0, 0.0, 0.0 }, { 7.0, 0.0, 0.0, 0.0 },
+			 	 	 	 	 	 	 	 { 12.0, 0.0, 0.0, 0.0 }, { 7.4, 0.0, 0.0, 0.0 },
 			 	 	 	 	 	 	 	 // Ambient
-			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.0, 0.0},
+			 	 	 	 	 	 	 	 { 0.1, 0.1, 0.1, 0.0},
 										 // Tenth sphere
 										 // Diffuse                 Specular
-										 { 1.0, 0.0, 0.0, 1.0 },  { 0.0, 1.0, 1.0, 1.0 },
+			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.9, 1.0 },  { 0.1, 0.1, 0.5, 1.0 },
 										 // Shininess               Translation
-			 	 	 	 	 	 	 	 { 250.0, 0.0, 0.0, 0.0 }, { 9.0, 0.0, 0.0, 0.0 },
+			 	 	 	 	 	 	 	 { 8.0, 0.0, 0.0, 0.0 }, { 9.5, 0.0, 0.0, 0.0 },
 			 	 	 	 	 	 	 	 // Ambient
-			 	 	 	 	 	 	 	 { 0.0, 0.0, 0.0, 0.0}
+			 	 	 	 	 	 	 	 { 0.1, 0.1, 0.1, 0.0},
 										};
 
-  for(int i = 0; i < 10; i++)
+  for(i = 0; i < 10; i++)
   {
   	j = i * 5;
   	sphere = new Sphere(data[j], data[j + 1], data[j + 2],
